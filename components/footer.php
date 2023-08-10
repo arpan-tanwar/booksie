8dbae1201d3f652947484e890bc8c8187106d99f
Arpan Tanwar <97386311+arpan-tanwar@users.noreply.github.com>
2023-08-10 06:31:43 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:+1705XXXXXX"><i class="fas fa-phone"></i> +1 (705) XXX-XXX</a>
+         <a href="tel:+1705XXXXXX"><i class="fas fa-phone"></i> +1 (705) XXX-XXX</a>
+         <a href="mailto:tanwara@uwindsor.ca"><i class="fas fa-envelope"></i> tanwara@uwindsor.ca</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Delhi, India - 110027 </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright by <span>Arpan Tanwar</span> | All Rights Reserved</div>
+
+</footer>
